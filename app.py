08662a85f280ce4efdb2c206cfa21d4655a66d58
Jason Co <jasonrobertco@gmail.com>
2025-11-30 08:48:50 -0800

    35 min

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+charactername = "John"
+bo = True
+phrase = "test"
+
+print(phrase.upper())
+print(phrase.isupper())
+
+
+print("Hello\n World")
+print("Hello" + charactername)
+print(charactername)
+
